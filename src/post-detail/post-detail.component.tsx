263d3df9c41baca7a3b6b2a121a83f28a633235f
Sahil <sahil.singh98@hotmail.com>
2024-02-08 20:11:38 +0100

    Add Favourite Functionality, save favourite Post to Array

--- src/post-detail/post-detail.component.tsx
+++ src/post-detail/post-detail.component.tsx
@@ -4,21 +4,24 @@ import { HttpService } from "../service/http.service";
 import { defaultPost } from "../models/post-dto";
 import moment from "moment";
 import { CommentsComponent } from "../comments/comments.component";
+import { LocalstorageService } from "../service/localstorage.service";
 
 export default function PostDetailComponent() {
   let { id } = useParams();
   let paramError = false;
   const httpService = new HttpService();
+  const localStorageService = new LocalstorageService();
   const [post, setPost] = useState(defaultPost);
   const [postId] = useState(Number(id));
   const [loading, setLoading] = useState(true);
+  const [isFavourite, setIsFavourite] = useState(false);
 
   useEffect(() => {
     fetchPostById().then(() => {});
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const fetchPostById = async () => {
+  const fetchPostById = async (): Promise<void> => {
     if (isNaN(postId)) {
       paramError = true;
       return;
@@ -27,12 +30,23 @@ export default function PostDetailComponent() {
     setPost(post);
 
     setLoading(false);
+    setIsFavourite(localStorageService.idExists(post.id));
   };
 
-  const getPostDate = (timestamp) => {
+  const getPostDate = (timestamp): string => {
     moment.locale("en");
     const date = new Date(timestamp);
     return moment(date).format("DD.MM.yyyy HH:mm");
+  };
+
+  const setToFavourite = (): void => {
+    if (localStorageService.idExists(post.id)) {
+      localStorageService.deletePost(post.id);
+      setIsFavourite(false);
+      return;
+    }
+    localStorageService.addPost(post);
+    setIsFavourite(true);
   };
 
   return (
@@ -57,6 +71,39 @@ export default function PostDetailComponent() {
       <div className="row mt-3">
         <div className="col-12">
           <div dangerouslySetInnerHTML={{ __html: post.body }}></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 d-flex justify-content-end">
+          <button className="btn btn-primary" onClick={setToFavourite}>
+            {isFavourite ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-heartbreak-fill"
+                viewBox="0 0 16 16"
+              >
+                {/* eslint-disable-next-line max-len */}
+                <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586M7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-heart-fill"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                />
+              </svg>
+            )}
+          </button>
         </div>
       </div>
       <div className="row mt-3">
---
+++ src/service/localstorage.service.ts
@@ -0,0 +1,34 @@
+import { PostDto } from "../models/post-dto";
+
+export class LocalstorageService {
+  private localStorageKey = "favourite_posts";
+
+  addPost(post: PostDto): void {
+    const posts = this.getPosts();
+    posts.push(post);
+    this.savePostsToLocalStorage(posts);
+  }
+
+  getPosts(): PostDto[] {
+    const postsJSON = localStorage.getItem(this.localStorageKey);
+    if (postsJSON) {
+      return JSON.parse(postsJSON);
+    }
+    return [];
+  }
+
+  deletePost(id: number): void {
+    let posts = this.getPosts();
+    posts = posts.filter((post) => post.id !== id);
+    this.savePostsToLocalStorage(posts);
+  }
+
+  idExists(id: number): boolean {
+    const posts = this.getPosts();
+    return posts.some((post) => post.id === id);
+  }
+
+  private savePostsToLocalStorage(posts: PostDto[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(posts));
+  }
+}
